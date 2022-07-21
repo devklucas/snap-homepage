0e596a9b8c8b129b7c49d5047d2f0afb62be346d
devklucas <lucascardoso64@gmail.com>
2022-07-21 17:10:27 -0300

    style: style global config

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+`
+const Home = () => {
+    return(
+        <Container>
+
+        </Container>
+    )
+}
+export default Home
---
+++ src/styles/global.tsx
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    *{
+        padding:0;
+        margin:0;
+        box-sizing: border-box;
+        font-family: 'Epilogue', sans-serif;
+    }
+    :root{
+        --almost-White: hsl(0, 0%, 98%);
+        --medium-Gray: hsl(0, 0%, 41%);
+        --almost-Black: hsl(0, 0%, 8%);
+    }
+    button{
+        outline:none;
+        cursor:pointer;
+    }
+`
