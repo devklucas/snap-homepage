ea3f750f2e4ced9915a593ea11575ab01d9e6864
devklucas <lucascardoso64@gmail.com>
2022-07-25 12:32:28 -0300

    style: style from desktop version

--- src/components/MenuMobile.tsx
+++ src/components/MenuMobile.tsx
@@ -10,6 +10,10 @@ import Calendar from '../assets/images/icon-calendar.svg'
 import Reminders from '../assets/images/icon-reminders.svg'
 import Planning from '../assets/images/icon-planning.svg'
 
+type Props = {
+    handleMenu?: () => void
+}
+
 const ContainerMenu = styled.aside`
     position:absolute;
     right:0;
@@ -19,8 +23,6 @@ const ContainerMenu = styled.aside`
     background-color: var(--almost-White);
     z-index: 1;
     color: var(--medium-Gray);
-    /* -webkit-box-shadow: -91px 0px 0px 0px rgba(20,20,20,0.46); */
-    /* -moz-box-shadow: -91px 0px 0px 0px rgba(20,20,20,0.46); */
     box-shadow: -200px 0px 0px 0px rgba(20,20,20,0.7);
     
     ul{
@@ -34,6 +36,24 @@ const ContainerMenu = styled.aside`
         border-radius:12px;
         border: 0.8px solid var(--medium-Gray);
     }
+    @media (min-width: 426px){
+        position:relative;
+        height:10vh;
+        width:100%;
+        display: flex;
+        box-shadow: none;
+        font-size:1rem;
+        ul{
+            display:flex;
+            align-items:center;
+            gap:20px;
+        }
+        li{
+            margin:0;
+            margin-left: 1rem;
+            cursor:pointer;
+        }
+    }
 `
 const Buttons = styled.button`
     width:90%;
@@ -42,12 +62,28 @@ const Buttons = styled.button`
     background-color: transparent;
     border:none;
     color: var(--medium-Gray);
+    @media (min-width: 426px){
+        margin:0;
+        width: 120px;
+        height:50%;
+        font-size: 1rem;
+    }
 `
 const Navigation = styled.nav`
         display:flex;
         flex-direction: column;
         justify-content:center;
         margin-top: 50px;
+    @media (min-width: 426px){
+        width:95%;
+        height:100%;
+        flex-direction: row;
+        align-items:center;
+        justify-content: space-between;
+        margin:0;
+        padding-left: 10px;
+        border:1px solid black;
+    }
 `
 const NavSelects = styled.li`
     nav{
@@ -57,7 +93,21 @@ const NavSelects = styled.li`
         margin-left: 20px;
         gap:15px;
     }
-  
+    @media (min-width: 426px){
+        position:relative;
+        nav{
+            position:absolute;
+            background-color:var(--almost-White); 
+            box-shadow: 0 0 1em var(--medium-Gray);  
+            padding:20px;
+            margin-top:10px;
+            margin-left:0px;
+        }
+        span{
+            display:flex;
+            align-items:center;
+        }
+    }
 ` 
 const OptionsSelects  = styled.button`
     border:none;
@@ -69,16 +119,23 @@ const OptionsSelects  = styled.button`
     img{
         margin-right:10px;
     }
+    @media (min-width: 426px){
+        width:max-content;
+        img{
+            margin-left: 10px;
+        }
+    }   
 `
 const BtnClose = styled.img`
     position: absolute;
     right:10px;
-    top:10px;   
+    top:10px;
+    @media (min-width: 426px){
+        display:none;
+    }   
 `
-type Props = {
-    handleMenu: () => void
-}
-export const MenuMobile = ({handleMenu} : Props) => {
+
+const MenuMobile = ({handleMenu} : Props) => {
     const [navFeatures, setNavFeatures] = useState(false)
     const [navCompany, setNavCompany] = useState(false)
 
@@ -126,6 +183,7 @@ export const MenuMobile = ({handleMenu} : Props) => {
         <Buttons>Login</Buttons>
         <Buttons className='BtnRegister'>Register</Buttons>
         </Navigation>
-    </ContainerMenu>  
+        </ContainerMenu>  
     )
 }
+export default MenuMobile
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import Logo from "../assets/images/logo.svg";
+import ImgLogo from "../assets/images/logo.svg";
 import Menu from "../assets/images/icon-menu.svg";
 
 import ArtMobile from "../assets/images/image-hero-mobile.png";
@@ -9,7 +9,7 @@ import Ads2 from "../assets/images/client-audiophile.svg";
 import Ads3 from "../assets/images/client-meet.svg";
 import Ads4 from "../assets/images/client-maker.svg";
 
-import { MenuMobile } from "../components/MenuMobile";
+import  MenuMobile  from "../components/MenuMobile";
 import { useState } from "react";
 
 const Container = styled.div`
@@ -22,22 +22,65 @@ const Header = styled.header`
   justify-content: space-between;
   align-items: center;
   padding: 1rem;
+  @media (min-width: 426px){
+    padding:0px;
+    height:10%;
+    .Menu-mobile{
+        display:none;
+    }   
+  }
+
 `;
-const MainContent = styled.main`
+const Logo = styled.img`
+   @media (min-width: 426px){
+      width:7%;
+      padding: 10px;
+   }
+`
+const BoxContent = styled.main`
+    width:100%;
+    height: calc(100% - 10%);
+    @media (min-width: 426px){
+        align-items:center;
+        justify-content: space-evenly;
+        display:flex;
+       
+    }
+`
+const MainContent = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   margin: 15px 0;
   gap:15px;
+  order:1;
+   @media (min-width: 426px){
+    width:40%; 
+    height: 70%;
+    align-items:flex-start;
+    border:1px solid black;
+    justify-content: space-between;
+  }
 `;
 const BoxImage = styled.section`
   background: url(${ArtMobile}) no-repeat;
   background-size: 100% 100%; 
   width: 100%;
   height: 45%;
+  @media (min-width: 426px){
+    background: url(${ArtDesktop}) no-repeat;
+    height: 80%;
+    width: 30%;
+    background-size: 100% 100%;
+    order:2; 
+  }
 `;
 const Title = styled.h1`
   font-size: 1.8rem;
+  @media (min-width: 426px){
+      font-size: 4rem;
+      text-align:left;
+   }
 `;
 const TextParagraph = styled.p`
   font-size: 0.8rem;
@@ -46,6 +89,14 @@ const TextParagraph = styled.p`
   text-align: center;
   margin:0 40px;
   line-height: 20px;
+  @media (min-width: 426px){
+      width:80%;
+      font-size: 1rem;
+      line-height: normal;
+      text-align:left;
+      margin:0;
+      border:1px solid black;
+   }
 `;
 const BtnLearnMore = styled.button`
   background-color: var(--almost-Black);
@@ -53,6 +104,15 @@ const BtnLearnMore = styled.button`
   border-radius: 10px;
   color: var(--almost-White);
   padding: 10px;
+  @media (min-width: 426px){
+    font-size:1rem;
+    padding:15px;
+    :hover{
+      background-color:var(--almost-White);
+      color: var(--almost-Black);
+      border:0.8px solid var(--almost-Black)
+    }
+  }
 `;
 const Footer = styled.footer`
   display: flex;
@@ -64,6 +124,10 @@ const Footer = styled.footer`
     width: 50px;
     height: 20px;
   }
+  @media (min-width: 426px){
+    border:1px solid black;
+    height: 10%;
+  }
 `;
 
 const Home = () => {
@@ -73,28 +137,31 @@ const Home = () => {
     return (
     <Container>
       <Header>
-        <img src={Logo} alt="Logmarca" />
-        <img src={Menu} alt="Icon menu" onClick={handleMenu}/>
+        <Logo src={ImgLogo} alt="Logmarca" />
+        <img src={Menu} alt="Icon menu" onClick={handleMenu} className='Menu-mobile'/>
+        <MenuMobile/>
       </Header>
+      <BoxContent>
       <BoxImage />
       <MainContent>
-        <Title>Make a remote work</Title>
+        <Title>Make remote work</Title>
         <TextParagraph>
           Get your team in sync, no matter your
           location. Streamline processes,
           create team rituals, and watch productivity soar.
         </TextParagraph>
         <BtnLearnMore>Learn More</BtnLearnMore>
-      </MainContent>
-      <Footer>
+        <Footer>
       <img src={Ads1} alt='ads'/> 
       <img src={Ads2} alt='ads'/> 
       <img src={Ads3} alt='ads'/> 
       <img src={Ads4} alt='ads'/> 
-      </Footer>
+        </Footer>
+      </MainContent>
        {    openMenu === true && 
            <MenuMobile handleMenu={handleMenu}/>  
         }
+        </BoxContent>
     </Container>
   );
 };
